270059cbe2fcd2f7b69b7fc002f845cc1c5d99eb
Hussain <hussainabuwala.1997@gmail.com>
2022-06-13 17:27:19 -0400

    Added Internet User Permission to AndroidManifest.xml Added corouting, okhttp and gson dependencies to build.gradle Added Feature to get Product Details from OpenFoodFacts API

--- app/src/main/java/com/example/ecoconnect/ProductDetailsActivity.kt
+++ app/src/main/java/com/example/ecoconnect/ProductDetailsActivity.kt
@@ -3,8 +3,17 @@ package com.example.ecoconnect
 import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.ecoconnect.databinding.ActivityMainBinding
 import com.example.ecoconnect.databinding.ActivityProductDetailsBinding
+import com.google.gson.GsonBuilder
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
 class ProductDetailsActivity : AppCompatActivity() {
 
@@ -21,8 +30,38 @@ class ProductDetailsActivity : AppCompatActivity() {
 
         binding.ivPhoto.setImageBitmap(scannedImg)
         binding.tvScanResult.text = "Barcode: $barcode"
+        GlobalScope.launch(Dispatchers.IO) {
+            // ...
+            find_product_info("3017620422003")
+        }
 
     }
 
+    suspend fun find_product_info(barcode: String?) {
+
+        val client = OkHttpClient()
+        val url = "https://world.openfoodfacts.org/api/v2/product/$barcode.json?fields=packagings,ecoscore_grade"
+        val request = Request.Builder()
+            .url(url)
+            .header("Content-Type", "application/json")
+            .build()
+        val response = client.newCall(request).execute()
+        val json = response.body().string()
+        val gson = GsonBuilder().create()
+        val result = gson.fromJson(json, Product::class.java)
+
+        result?.product?.ecoscore_grade?.let {
+            Log.d("find_product_info","Ecoscore_grade: $it")
+            withContext(Dispatchers.Main){
+                Toast.makeText(this@ProductDetailsActivity, "Ecoscore_grade: $it",Toast.LENGTH_LONG).show()
+            }
+        }
+
+        result?.product?.packagings?.let {
+            it.forEach{
+                Log.d("find_product_info","Material: ${it.material}, Recycling: ${it.recycling}, Shape: ${it.shape}")
+            }
+        }
+    }
 
 }
